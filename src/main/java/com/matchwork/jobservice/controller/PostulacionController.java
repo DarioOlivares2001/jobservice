79e9e715a18173ea6aab7fdc1810bed60bd894d8
DarioOlivares2001 <olivaresdario2001@gmail.com>
2025-06-24 16:51:14 -0400

    ultimos cambios para paso a prod

--- src/main/java/com/matchwork/jobservice/controller/PostulacionController.java
+++ src/main/java/com/matchwork/jobservice/controller/PostulacionController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/postulaciones")
-@CrossOrigin(origins = "*")
 public class PostulacionController {
 
     private final PostulacionService postulacionService;
